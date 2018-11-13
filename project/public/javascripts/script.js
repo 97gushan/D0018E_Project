138f16d6f5da7375ec51481029644196a5f62522
PandaFood <jonathan.brorsson@gmail.com>
2018-11-13 13:09:21 +0100

    Added simple front-end view

--- project/public/javascripts/script.js
+++ project/public/javascripts/script.js
@@ -1,2 +1,16 @@
 
-alert("Detta funkar till och med - Brought to you by 'public/javascripts/scripts.js linked by index.ejs");
+//alert("Detta funkar till och med - Brought to you by 'public/javascripts/scripts.js linked by index.ejs");
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("banner");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
