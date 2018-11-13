5a59962c7a169e21df2c55e2653e28bfd9d77c07
97gushan <97gustavh@gmail.com>
2018-11-13 10:10:41 +0100

    Created a route file which calls Database.js and its 'addUser method', addUser only prints to the console at the moment

---
+++ project/Database.js
@@ -0,0 +1,16 @@
+console.log("Database.js");
+
+
+module.exports = {
+    addUserToDB : function(){
+        console.log("Adding user -- NOT DONE");
+        
+    }, addProduct : function() {
+        console.log("Adding product -- NOT DONE");
+
+    }
+  };
+
+
+
+
--- project/app.js
+++ project/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addUserRouter = require('./routes/addUser');
 
 var app = express();
 
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wubwub', addUserRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
