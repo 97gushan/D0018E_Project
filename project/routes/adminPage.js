f55e7f51b51f485dd486b0bd05148198bfdc2d70
97gushan <97gustavh@gmail.com>
2018-11-15 16:22:31 +0100

    renamed adminpanel to a more uniform style

--- project/routes/adminPage.js
+++ project/routes/adminPage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Adminpanel', { title: 'Express' });
+  res.render('adminPanel', { title: 'Express' });
 });
 
 module.exports = router;
