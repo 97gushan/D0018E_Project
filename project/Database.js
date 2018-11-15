d047620665c3c03ae2b964bca64d60d5fc96aa55
albmar-6 <albin.martinsson92@gmail.com>
2018-11-15 12:32:30 +0100

    Added connection function

--- project/Database.js
+++ project/Database.js
@@ -1,5 +1,18 @@
 console.log("Database.js");
+var mysql = require('mysql')
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: '3306',
+  password: '',
+  database: 'D0018E'
+})
+
+-*connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})-* 
 
 module.exports = {
     addUserToDB : function(){
