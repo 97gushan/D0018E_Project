4972829be74f5b8711468e325a374e3252a222a5
97gushan <97gustavh@gmail.com>
2018-11-20 11:41:01 +0100

    removed addUser route and added bcrypt dependency

--- project/app.js
+++ project/app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var addUserRouter = require('./routes/addUser');
 var bodyParser = require('body-parser');
 var apiRouter = require('./routes/api');
+var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/addUser', addUserRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
