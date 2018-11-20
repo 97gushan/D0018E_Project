f4c0e0212e284b5c06f2f22e92bfb86ce5270502
PandaFood <jonathan.brorsson@gmail.com>
2018-11-20 11:47:29 +0100

    Added a bit of product handling

--- project/public/javascripts/script.js
+++ project/public/javascripts/script.js
@@ -2,15 +2,64 @@
 //alert("Detta funkar till och med - Brought to you by 'public/javascripts/scripts.js linked by index.ejs");
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyHeader()};
 
 var header = document.getElementById("banner");
 var sticky = header.offsetTop;
 
-function myFunction() {
+function stickyHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
+
+$(document).ready(function(){
+
+  // Anonymous function to handle adding a product box
+  var productbox = (name, description, price) => {
+  
+    var baseText = `
+    <div class="box">
+      <div>
+        <h2>${name}</h2> 
+        <div> 
+          <div> 
+            <p>${description}</p>
+          </div>
+          <p>${price} kr</p>
+        </div>
+      </div>
+    </div> `
+
+  return baseText
+  };
+
+  // TODO: Add database query for this instead of 
+  // logging the value.
+  $("#inputname").focus(function(){
+    $("#inputname").keyup(function(){
+      console.log($("#inputname").val());
+    });
+  });
+
+  // Temporary adding of product.
+  //    This is how we add the products
+  $("#inputbutton").click(function(){
+    $(".productbox-container").append(productbox($("#inputname").val(), "A generic product\u2122" ,10));
+  });
+
+
+});
+
+
+function getProducts() {
+
+}
+
+var product = {
+  name:"",
+  description:"",
+  price:""
+};
