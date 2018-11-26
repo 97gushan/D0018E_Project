57d153733fdd05c5176586d3938bda991e216f4f
PandaFood <jonathan.brorsson@gmail.com>
2018-11-26 15:54:44 +0100

    Added frontend product fetching + fixed some async code

--- project/public/javascripts/script.js
+++ project/public/javascripts/script.js
@@ -1,7 +1,4 @@
 
-//alert("Detta funkar till och med - Brought to you by 'public/javascripts/scripts.js linked by index.ejs");
-
-
 window.onscroll = function() {stickyHeader()};
 
 var header = document.getElementById("banner");
@@ -18,35 +15,38 @@ function stickyHeader() {
 $(document).ready(function(){
 
 
-  // TODO: Add database query for this instead of 
-  // logging the value.
   $("#inputname").focus(function(){
-    $("#inputname").keyup(function(){
-      console.log($("#inputname").val());
-    });
+    searchBarHandler();
   });
-
-  // Temporary adding of product.
-  //    This is how we add the products
-  $("#getterbutton").click(function(){
-    
-    $.getJSON( "/api/ex/json", function(jsonfile) {
-      getProducts(jsonfile);
-    });
-  });
-
 
 });
+
+function searchBarHandler(){
+  var writetime = 600;
+  var sr = setTimeout(() => {
+    getProducts();
+  }, writetime);
+
+  function resetTimeout(timeoutObject){
+    clearTimeout(timeoutObject);
+    return setTimeout(() => { getProducts(); }, writetime);
+  }
+
+  $("#inputname").keyup(function(){
+    resetTimeout(sr);
+  });
+  
+}
 
 
 
 
 
 // Anonymous function to handle adding a product box
-var productbox = (name, description, price) => {
+var productbox = (id, name, description, price) => {
 
   var baseText = `
-  <div class="box">
+  <div class="box" id="productBoxnr${id}">
     <div>
       <h2>${name}</h2> 
       <div> 
@@ -62,22 +62,34 @@ return baseText
 };
 
 // Product object
-var product = {
-  name:"",
-  description:"",
-  price:""
+function product(id, name, description, price){
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.price = price;
 };
 
+var products = [];
 
-function getProducts(file) {
-  console.log(file);
-  file.products.forEach(prod => {
-    addProduct(prod);
+
+// Get the products with the chosen filter
+//    and call the function to add the products
+//    to the website.
+function getProducts() {
+  $.getJSON("/api/product/get?query=" + $("#inputname").val(), function(jsonfile) {
+    products = [];
+    jsonfile.forEach(prod => {
+      products.push(new product(prod.id, prod.name, prod.description, prod.price));
+    });
+    addProducts();
   });
-
+  
 }
 
-function addProduct(prod){
-  $(".productbox-container").append(productbox(prod.name , prod.description , prod.price));
+function addProducts(){
+  $(".productbox-container").empty();
+  products.forEach(prod => {
+    $(".productbox-container").append(productbox(prod.id, prod.name , prod.description , prod.price));
+  })
 }
 
